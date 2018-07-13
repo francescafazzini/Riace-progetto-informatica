eb5bdc9c7e23cc5fe80ccf5c9891db10e07f10d8
Francesca Fazzini <francesca180998@gmail.com>
2018-07-13 15:20:40 +0100

    slideshow

--- script.js
+++ script.js
@@ -1,36 +1,43 @@
-var Slider = new function() {
+var i = 0; 			// Start Point
+var images = [];	// Images Array
+var time = 5000;	// Time Between Switch
 
-	var index = -1;
-	var wrapper = $('#slideshow-wrapper', '#slideshow');
-	var images = $('img', wrapper);
+// Image List
+images[0] = <img src="0.svg">
+images[2] = <img src="1.svg">
+images[1] = <img src="2.svg">
+images[4] = <img src="3.svg">
+images[3] = <img src="4.svg">
+images[5] = <img src="5.svg">
+images[6] = <img src="6.svg">
+images[7] = <img src="7.svg">
+images[8] = <img src="8.svg">
+images[9] = <img src="9.svg">
+images[10] = <img src="10.svg">
+images[11] = <img src="10.svg">
+images[12] = <img src="12.svg">
+images[13] = <img src="13.svg">
+images[14] = <img src="14.svg">
+images[16] = <img src="15.svg">
+images[15] = <img src="16.svg">
+images[17] = <img src="17.svg">
 
-	var slide = setInterval(function() {
+// Change Image
+function changeImg(){
+	document.slide.src = images[i];
 
-		index++;
+	// Check If Index Is Under Max
+	if(i < images.length - 1){
+	  // Add 1 to Index
+	  i++;
+	} else {
+		// Reset Back To O
+		i = 0;
+	}
 
-		if(index == images.length) {
+	// Run function every x seconds
+	setTimeout("changeImg()", time);
+}
 
-			index = 0;
-
-			images.fadeIn();
-
-		}
-
-		images.eq(index).fadeOut(2000);
-
-
-	}, 2000);
-
-	this.init = function() {
-
-		slide();
-
-	};
-
-}();
-
-$(function() {
-
-	Slider.init();
-
-});
+// Run function when page loads
+window.onload=changeImg;
