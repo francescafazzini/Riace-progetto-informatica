e52b9ff1019bd3f20882a9d9d212db4335c62a62
Francesca Fazzini <francesca180998@gmail.com>
2018-08-25 15:21:19 +0100

    menu ok

---
+++ README - Copia.md
@@ -0,0 +1,1 @@
+# Riace-progetto-informatica
---
+++ script - Copia.js
@@ -0,0 +1,6 @@
+<script>
+var myParagrafo = document.getElementById("box1");
+
+myParagrafo.onmouseover = function(e) { e.target.className = "overClass"; };
+myParagrafo.onmouseout  = function(e) { e.target.className = "outClass";  };
+</script>
